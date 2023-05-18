5ac2ab1c9cd626b861776885d2a70a9271f7a19e
Siddhant Parekh <parekhsiddhant@gmail.com>
2023-05-19 01:52:23 +0530

    Exposed keys handled

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,34 +1,38 @@
 const prod = process.env.NODE_ENV === "production";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-	mode: prod ? "production" : "development",
-	entry: "./src/index.tsx",
-	output: {
-		path: __dirname + "/dist/",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				resolve: {
-					extensions: [".ts", ".tsx", ".js", ".json"],
-				},
-				use: "ts-loader",
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
-			},
-		],
-	},
-	devtool: prod ? undefined : "source-map",
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "index.html",
-		}),
-		new MiniCssExtractPlugin(),
-	],
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      "process.env.REACT_APP_ENV": JSON.stringify(process.env.REACT_APP_ENV),
+    }),
+  ],
 };
